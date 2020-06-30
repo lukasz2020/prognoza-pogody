de8e3377de9b1aaaf0f471f7e28d91f58399cd9a
lukasz2020 <lukl1@wp.pl>
2020-06-30 22:54:30 +0200

    Updates

---
+++ precache-manifest.c3e6a487f04a22b61f333f59721aedc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b89ef12efb2753daf1c3215af09d882",
+    "url": "/prognoza-pogody/index.html"
+  },
+  {
+    "revision": "88e002c7882415c3a022",
+    "url": "/prognoza-pogody/static/js/2.a78abd11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prognoza-pogody/static/js/2.a78abd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e61d04c6d379c724e6",
+    "url": "/prognoza-pogody/static/js/main.da4e6e89.chunk.js"
+  },
+  {
+    "revision": "bd8a9fb32e4cd72ea1cf",
+    "url": "/prognoza-pogody/static/js/runtime-main.6654ba9b.js"
+  }
+]);
